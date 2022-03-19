64d34126354d0cebcc17287c8313f22b3fd1fa5f
Kae <73745621+Kae-Tempest@users.noreply.github.com>
2022-03-19 22:11:45 +0100

    Add Twitch annoncing

--- Root/Events/ReadyClient.js
+++ Root/Events/ReadyClient.js
@@ -1,6 +1,9 @@
 const chalk = require("chalk")
 const {options} = require('../../Config')
 const fetch = require('node-fetch');
+const {MessageEmbed} = require("discord.js");
+const {capitalize} = require("../function/other/string");
+const http = require("http");
 module.exports = {
     name: "ready",
     once: true,
@@ -17,16 +20,50 @@ module.exports = {
         if (client.commands.slashCommands.size > 0) console.log(chalk.bold.red("[Handler]") + chalk.bold.yellow(` Found ${client.commands.slashCommands.size} slashCommands. Starting to load.`))
         if (client.commands.contextMenus.size > 0) console.log(chalk.bold.greenBright("[Handler]") + chalk.bold.cyanBright(` Found ${client.commands.contextMenus.size} contextMenus. Starting to load.`))
 
-        let twitchAPI = 'https://api.twitch.tv/helix/streams?user_login=tanuky_&user_login=shyza_of&user_login=kaetempest';
+        let StreamOnList = [];
+        let streamer = await client.getAllStreamerName();
         (TwitchData = async () => {
             setInterval(async () => {
+                let twitchAPI = `https://api.twitch.tv/helix/streams?user_login=${streamer.map(n => n).join('&user_login=')}`;
                 let res = await fetch(twitchAPI, options);
                 let data = await res.json();
                 sendEmbed(data)
             }, 20000)
         })()
-        let sendEmbed = (data) => {
-            console.log(data.data)
+        let sendEmbed = async (data) => {
+            let streamList = data.data
+            if(streamList === undefined) return
+            for (let i = 0; streamList.length > i; i++) {
+                if (!StreamOnList.find(stream => stream === streamList[i]['user_login'])) {
+                    let streamerGuilds = await client.getStreamer(streamList[i]['user_login'])
+                    let Guilds = JSON.parse(streamerGuilds['GUILD'])
+                    let res = await fetch(`https://api.twitch.tv/helix/users?login=${streamList[i]['user_login']}`, options);
+                    let StreamerData = await res.json();
+                    StreamOnList.push(streamList[i]['user_login'])
+                    let imgstream = streamList[i]['thumbnail_url'].slice(0, streamList[i]['thumbnail_url'].length - 20)
+                    const embed = new MessageEmbed()
+                        .setAuthor({name: `${streamList[i]['user_name']} est en stream`, iconURL: StreamerData.data[0]['profile_image_url']})
+                        .setThumbnail(StreamerData.data[0]['profile_image_url'])
+                        .setDescription(`[${streamList[i]['title']}](https://www.twitch.tv/${streamList[i]['user_login']})`)
+                        .addField(`Viewers`, `${streamList[i]['viewer_count']}`, true)
+                        .setImage(`${imgstream}320x180.jpg`)
+                        .setTimestamp()
+                    for(let i = 0 ; Guilds.length > i ; i++){
+                        let GuildInfo = await client.getGuild(Guilds[i])
+                        client.channels.resolve(GuildInfo['LogChannel']).send({
+                            embeds: [embed]
+                        })
+                    }
+
+                }
+            }
+            if(StreamOnList !== []) {
+                for (let i = 0; StreamOnList.length > i; i++) {
+                    if (!streamList.find(stream => stream['user_login'] === StreamOnList[i])) {
+                        StreamOnList.splice(i, 1)
+                    }
+                }
+            }
         }
     }
 }
