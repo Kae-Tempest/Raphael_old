e9bd35bd6b64414791c1160b81a660a9f6ae31d8
Kae <73745621+Kae-Tempest@users.noreply.github.com>
2022-04-01 21:53:28 +0200

    Revert "Revert "Trade Cmd""
    This reverts commit de2eb53bff39b2ee7d73f93f44fd2cd4241809e9.

---
+++ Root/Commands/ButtonCommands/DtradeY.js
@@ -0,0 +1,58 @@
+const {MessageEmbed, MessageActionRow, MessageSelectMenu} = require("discord.js");
+const {capitalize} = require("../../function/other/string");
+const Builders = require("@discordjs/builders");
+function createOptionSelectMenu(label, value) {
+    let MenuOption = new Builders.SelectMenuOption();
+    MenuOption.setLabel(label);
+    MenuOption.setValue(label);
+    return MenuOption;
+}
+module.exports = {
+    name : 'DtradeY',
+    run : async(client, button) => {
+        let player = client.users.cache.find(user => user.username === button.message.embeds[0].footer.text)
+        let user = await client.getAllDataUser(player)
+        let itemName = button.message.embeds[0].fields[0].value
+        let item = await client.getItem(itemName)
+        await player.createDM().then(async (message) => {
+            const itemEmbed = new MessageEmbed()
+                .setTitle(`${button.user.username} veux t'échanger \`${itemName}\``)
+                .addFields({name: 'Item:', value: itemName},
+                    {name: 'Stats:', value: `
+                Attaque : ${item['ATTAQUE']} | Constitution : ${item['CONSTITUTION']}
+                Agility : ${item['AGILITY']} | Esprit : ${item['ESPRIT']}
+                Intelligence : ${item['INTELLIGENCE']} | Vitality : ${item['VITALITY']}`}
+                )
+                .setTimestamp()
+                .setFooter({text: button.user.username})
+            let Inventory = await client.getAllUserInventory(user)
+            const itemList = []
+            for(let i = 0; Inventory.length > i; i++){
+                let item = await client.getItem(Inventory[i]['ITEM_NAME']);
+                itemList.push(createOptionSelectMenu(capitalize(item['ITEM_NAME'])))
+            }
+            if(itemList == false) return button.update({
+                content: 'Le joueur n\'as rien a vous echanger',
+                embeds: [],
+                components: []
+            })
+            const rows = new MessageActionRow().addComponents(
+                new MessageSelectMenu()
+                    .setCustomId('rtrade')
+                    .setPlaceholder('Choose item to trade')
+                    .setOptions(itemList)
+            )
+
+            await message.send({
+                content: `Vous avez reçu une demande d\'échange de ${button.user.username}`,
+                embeds: [itemEmbed],
+                components: [rows]
+            })
+            button.update({
+                content: `Demande d'échange envoyé à ${player.username}`,
+                embeds: [],
+                components: []
+            })
+        })
+    }
+}
---
+++ Root/Commands/ButtonCommands/RtradeY.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name : 'RtradeY',
+    run : async(client, button) => {
+        let user = button.user
+        let receiver = client.users.cache.find(user => user.username === button.message.embeds[0].footer.text)
+
+        await client.updateInventoryUserItem(button.message.embeds[0].fields[2].value,user, receiver)
+        await client.updateInventoryUserItem(button.message.embeds[0].fields[0].value,receiver, user)
+
+        await receiver.createDM().then(async (message) => {
+            await message.send({
+                content: `L'échange a bien été effectué`
+            })
+        })
+        await button.update({
+            content: `L'échange a bien été effectué`,
+            embeds: [],
+            components: []
+        })
+    }
+}
