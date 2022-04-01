7710eacf067cc81581e6d626548285ab051e35c0
Kae <73745621+Kae-Tempest@users.noreply.github.com>
2022-04-01 21:52:43 +0200

    Trade Cmd

---
+++ Root/Commands/ButtonCommands/RtradeN.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name : 'RtradeN',
+    run : async(client, button) => {
+        button.update({
+            content: `Vous avez refusez l'echange avec \`${button.message.embeds[0].footer.text}\``,
+            embeds: [],
+            components: []
+        })
+    }
+}
